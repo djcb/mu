3b253adf436482dea053ab9b43f5a65f89f90e50
Dirk-Jan C. Binnema <djcb@djcbsoftware.nl>
2010-02-02 21:47:19 +0200

    * test-mu-common.[ch]: centralize some common test code

---
+++ src/tests/test-mu-common.c
@@ -0,0 +1,41 @@
+/* 
+** Copyright (C) 2010 Dirk-Jan C. Binnema <djcb@djcbsoftware.nl>
+**
+** This program is free software; you can redistribute it and/or modify it
+** under the terms of the GNU General Public License as published by the
+** Free Software Foundation; either version 3, or (at your option) any
+** later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software Foundation,
+** Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  
+**  
+*/
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif /*HAVE_CONFIG_H*/
+
+#include <glib.h>
+#include <glib/gstdio.h>
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "test-mu-common.h"
+
+char*
+test_mu_common_get_random_tmpdir (void)
+{
+        return g_strdup_printf ("%s%cmu-test-%d%ctest-%x", g_get_tmp_dir(),
+				G_DIR_SEPARATOR,
+				getuid(),
+                                G_DIR_SEPARATOR,
+                                (int)random()*getpid()*(int)time(NULL));
+}
