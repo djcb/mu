3cc7d4ee8947fb0be529bb999a098655452669bf
Dirk-Jan C. Binnema <djcb@djcbsoftware.nl>
2010-09-09 08:15:41 +0300

    * mu.cc: minor changes

--- src/mu.cc
+++ src/mu.cc
@@ -1,5 +1,5 @@
 /*
-** Copyright (C) 2010 Dirk-Jan C. Binnema <djcb@djcbsoftware.nl>
+** Copyright (C) 2008-2010 Dirk-Jan C. Binnema <djcb@djcbsoftware.nl>
 **
 ** This program is free software; you can redistribute it and/or modify it
 ** under the terms of the GNU General Public License as published by the
@@ -17,7 +17,9 @@
 **
 */
 
+#ifdef HAVE_CONFIG_H
 #include <config.h>
+#endif /*HAVE_CONFIG_H*/
 
 #include <glib.h>
 #include <stdio.h> /* for fileno() */
