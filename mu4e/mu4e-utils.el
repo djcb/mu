849cc796368ab36c97afd3a01a181dc654aa729b
djcb <djcb@djcbsoftware.nl>
2012-08-07 12:08:44 +0300

    * mu4e-utils: quote names when autocompleting addresses

--- mu4e/mu4e-utils.el
+++ mu4e/mu4e-utils.el
@@ -580,7 +580,7 @@ This is used by the completion function in mu4e-compose."
 	    (and mu4e-compose-complete-ignore-address-regexp
 	      (string-match mu4e-compose-complete-ignore-address-regexp mail))
 	  (add-to-list 'lst
-	    (if name (format "%s <%s>" name mail) mail))))))
+	    (if name (format "\"%s\" <%s>" name mail) mail))))))
     (setq mu4e~contacts-for-completion lst)
     (mu4e-message "Contacts received: %d"
       (length mu4e~contacts-for-completion))))
