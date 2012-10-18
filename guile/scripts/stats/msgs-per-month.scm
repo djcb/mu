6d19880cedcd1d0db802014399dc714c1b781761
djcb <djcb@djcbsoftware.nl>
2012-10-19 00:49:14 +0300

    * guile: add a bunch of statistics scripts

---
+++ guile/scripts/stats/msgs-per-month.scm
@@ -0,0 +1,43 @@
+#!/bin/sh
+exec guile -e main -s $0 $@
+!#
+;;
+;; Copyright (C) 2012 Dirk-Jan C. Binnema <djcb@djcbsoftware.nl>
+;;
+;; This program is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the
+;; Free Software Foundation; either version 3, or (at your option) any
+;; later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software Foundation,
+;; Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+(use-modules (mu) (mu script) (mu stats) (mu plot))
+
+(define (per-month expr text-only)
+  "Count the total number of messages for each weekday (0-6 for
+Sun..Sat) that match EXPR. If PLAIN-TEXT is true, use a plain-text
+display, otherwise, use a graphical window."
+  (mu:plot
+    (mu:month-numbers->names
+      (sort
+	(mu:tabulate
+	  (lambda (msg)
+	    (tm:mon (localtime (mu:date msg)))) expr)
+	(lambda (x y) (< (car x) (car y)))))
+    (format #f "Messages per month matching ~a" expr)
+    "Month" "Messages" text-only))
+
+(define (main args)
+  (mu:run args per-month))
+
+;; Local Variables:
+;; mode: scheme
+;; End:
---
+++ guile/scripts/stats/msgs-per-year-month.scm
@@ -0,0 +1,45 @@
+#!/bin/sh
+exec guile -e main -s $0 $@
+!#
+;;
+;; Copyright (C) 2012 Dirk-Jan C. Binnema <djcb@djcbsoftware.nl>
+;;
+;; This program is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the
+;; Free Software Foundation; either version 3, or (at your option) any
+;; later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software Foundation,
+;; Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+(use-modules (mu) (mu script) (mu stats) (mu plot))
+
+(define (per-year-month expr text-only)
+  "Count the total number of messages for each weekday (0-6 for
+Sun..Sat) that match EXPR. If PLAIN-TEXT is true, use a plain-text
+display, otherwise, use a graphical window."
+  (mu:plot
+    (sort (mu:tabulate
+	    (lambda (msg)
+	      (string->number
+		(format #f "~d~2'0d"
+		  (+ 1900 (tm:year (localtime (mu:date msg))))
+		  (tm:mon (localtime (mu:date msg))))))
+	    expr)
+      (lambda (x y) (< (car x) (car y))))
+    (format #f "Messages per year/month matching ~a" expr)
+    "Year/Month" "Messages" text-only))
+
+(define (main args)
+  (mu:run args per-year-month))
+
+;; Local Variables:
+;; mode: scheme
+;; End:
