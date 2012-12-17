3353e32c28e9226a4aa09944453bba2fbd76c029
djcb <djcb@djcbsoftware.nl>
2012-12-17 22:30:07 +0200

    * add skipping support to mu-find

--- mu/mu-config.h
+++ mu/mu-config.h
@@ -139,7 +139,7 @@ struct _MuConfig {
 	gchar		*exec;		/* command to execute on the
 					 * files for the matched
 					 * messages */
-	gboolean        no_msgid_dups;  /* if there are multiple
+	gboolean        no_dups;        /* if there are multiple
 					 * messages with the same
 					 * msgid, show only the first
 					 * one */
