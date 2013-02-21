55619550e9d7934bb1fc2fe460893afbef64b730
Sasha Kovar <sasha-git@arcocene.org>
2013-02-21 11:11:03 -0800

    mu4e: propertize parens around attachment size in message view

--- mu4e/mu4e-view.el
+++ mu4e/mu4e-view.el
@@ -437,9 +437,8 @@ at POINT, or if nil, at (point)."
 			"[mouse-1] or [M-RET] opens the attachment\n"
 			"[mouse-2] or [S-RET] offers to save it"))
 		    (when (and size (> size 0))
-		      (concat (format "(%s)"
-				(propertize (mu4e-display-size size)
-				  'face 'mu4e-view-header-key-face)))))))
+		      (propertize (format "(%s)" (mu4e-display-size size))
+                                  'face 'mu4e-view-header-key-face)))))
 	      attachments ", ")))
     (when attachments
       (mu4e~view-construct-header :attachments attstr t))))
