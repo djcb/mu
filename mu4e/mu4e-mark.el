ad8488368b3c605365dcf33649edca17107d4ef8
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2014-11-27 10:16:57 +0100

    always recompute the dynamic target for a mark
    This is a bug fix. Previously, recomputing was done only for refile,
    which is wrong: trash target can also be dynamic, and we want to allow
    the user to configure more dynamic targets.

--- mu4e/mu4e-mark.el
+++ mu4e/mu4e-mark.el
@@ -294,8 +294,7 @@ headers in the region. Optionally, provide TARGET (for moves)."
       (let ((cant-go-further) (eor (region-end)))
 	(goto-char (region-beginning))
 	(while (and (<= (point) eor) (not cant-go-further))
-	  (when (eq mark 'refile)
-            (setq target (mu4e~mark-get-target mark target)))
+          (setq target (mu4e~mark-get-target mark target))
 	  (mu4e-mark-at-point mark target)
 	  (setq cant-go-further (not (mu4e-headers-next))))))))
 
