105809cd8e214c8bf4d2c81a2d08a0b7f0fcd2d5
djcb <djcb@djcbsoftware.nl>
2015-10-19 12:13:21 +0300

    mu4e: Use length, not string-width when fontifying body
    The latter does not what we want and gives errors (esp. on OSX it seems)

--- mu4e/mu4e-view.el
+++ mu4e/mu4e-view.el
@@ -242,7 +242,7 @@ found."
     "\n"
     (let ((body (mu4e-message-body-text msg)))
       (when (fboundp 'add-face-text-property)
-        (add-face-text-property 0 (string-width body) 'mu4e-view-body-face t body))
+        (add-face-text-property 0 (length body) 'mu4e-view-body-face t body))
       body)))
 
 (defun mu4e~view-embedded-winbuf ()
