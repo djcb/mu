90d2b8a1fa5d5f07127ab10dfcbb46982bac8f9b
djcb <djcb@djcbsoftware.nl>
2015-12-15 10:42:24 +0200

    mu4e: some less fancy fancy chars
    Try to find some fancy chars that are more universally available.

--- mu4e/mu4e-mark.el
+++ mu4e/mu4e-mark.el
@@ -122,13 +122,13 @@ is either a headers or view buffer."
 
 (defvar mu4e-marks
     '((refile
-	:char ("r" . "▶")
+	:char ("r" . "⯈")
 	:prompt "refile"
 	:dyn-target (lambda (target msg) (mu4e-get-refile-folder msg))
 	:action (lambda (docid msg target) (mu4e~proc-move docid
 						  (mu4e~mark-check-target target) "-N")))
        (delete
-	 :char ("D" . "🗙")
+	 :char ("D" . "⭙")
 	 :prompt "Delete"
 	 :show-target (lambda (target) "delete")
 	 :action (lambda (docid msg target) (mu4e~proc-remove docid)))
@@ -138,13 +138,13 @@ is either a headers or view buffer."
 	 :show-target (lambda (target) "flag")
 	 :action (lambda (docid msg target) (mu4e~proc-move docid nil "+F-u-N")))
        (move
-	 :char ("m" . "▷")
+	 :char ("m" . "⩥")
 	 :prompt "move"
 	 :ask-target  mu4e~mark-get-move-target
 	 :action (lambda (docid msg target) (mu4e~proc-move docid
 					      (mu4e~mark-check-target target) "-N")))
        (read
-	 :char    ("!" . "☑")
+	 :char    ("!" . "⬛")
 	 :prompt "!read"
 	 :show-target (lambda (target) "read")
 	 :action (lambda (docid msg target) (mu4e~proc-move docid nil "+S-u-N")))
@@ -160,12 +160,12 @@ is either a headers or view buffer."
 	 :show-target (lambda (target) "unflag")
 	 :action (lambda (docid msg target) (mu4e~proc-move docid nil "-F-N")))
        (untrash
-	 :char   ("=" . "🡸")
+	 :char   ("=" . "⯇")
 	 :prompt "=untrash"
 	 :show-target (lambda (target) "untrash")
 	 :action (lambda (docid msg target) (mu4e~proc-move docid nil "-T")))
        (unread
-	 :char    ("?" . "☐")
+	 :char    ("?" . "⬜")
 	 :prompt "?unread"
 	 :show-target (lambda (target) "unread")
 	 :action (lambda (docid msg target) (mu4e~proc-move docid nil "-S+u-N")))
@@ -174,7 +174,7 @@ is either a headers or view buffer."
 	 :prompt "unmark"
 	 :action (mu4e-error "No action for unmarking"))
        (action
-	 :char ( "a" . "🗲")
+	 :char ( "a" . "⮹")
 	 :prompt "action"
 	 :ask-target  (lambda () (mu4e-read-option "Action: " mu4e-headers-actions))
 	 :action  (lambda (docid msg actionfunc)
