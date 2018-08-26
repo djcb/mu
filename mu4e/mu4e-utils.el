48b241bef5bedbb881988f2b750a2597fc7c20f6
Juergen Hoetzel <juergen@archlinux.org>
2018-08-26 14:31:00 +0200

    mu4e: Don't descend in maildirs when recursively getting maildirs
    maildirs are already the leaf nodes in the folder structure.
    Also reduce number of intermediate (garbage collected) values.

--- mu4e/mu4e-utils.el
+++ mu4e/mu4e-utils.el
@@ -323,15 +323,14 @@ Function will return the cdr of the list element."
 	     (directory-files-and-attributes
 	       (concat path mdir) nil
 	       "^[^.]\\|\\.[^.][^.]" t))))
-    (dolist (dentry dentries)
-      (when (and (booleanp (cadr dentry)) (cadr dentry))
+    (dolist (dentry dentries dirs)
+      (when (eq (cadr dentry) t)
 	(if (file-accessible-directory-p
-	      (concat mu4e-maildir "/" mdir "/" (car dentry) "/cur"))
-	  (setq dirs (cons (concat mdir (car dentry)) dirs)))
-	(unless (member (car dentry) '("cur" "new" "tmp"))
-	  (setq dirs (append dirs (mu4e~get-maildirs-1 path
-				    (concat mdir (car dentry) "/")))))))
-    dirs))
+	     (concat mu4e-maildir "/" mdir "/" (car dentry) "/cur"))
+	    (push (concat mdir (car dentry)) dirs)
+	  (unless (member (car dentry) '("cur" "new" "tmp"))
+	    (setq dirs (append dirs (mu4e~get-maildirs-1 path
+							 (concat mdir (car dentry) "/"))))))))))
 
 (defvar mu4e-cache-maildir-list nil
   "Whether to cache the list of maildirs; set it to t if you find
