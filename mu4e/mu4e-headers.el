1acadb09dac91cd2064c6f37763806d8e686f5bd
Giuseppe Fierro <gspe@ae-design.ws>
2020-05-11 15:58:09 +0200

    mu4e: fix trashed messages face
    This fix the look and feel of trashed messages:
    - messages moved to trash folder with trashed flag
    - messages moved to trash folder but without trashed flag

--- mu4e/mu4e-headers.el
+++ mu4e/mu4e-headers.el
@@ -703,8 +703,12 @@ found."
 (defun mu4e~headers-line-apply-flag-face (msg line)
   "Adjust LINE's face property based on FLAGS."
   (let* ((flags (mu4e-message-field msg :flags))
+         (msg-dir (mu4e-message-field msg :maildir))
+         (trash-dir (mu4e-get-trash-folder msg))
          (face (cond
-                ((memq 'trashed flags) 'mu4e-trashed-face)
+                ((or (memq 'trashed flags)
+                     (equal trash-dir msg-dir))
+                 'mu4e-trashed-face)
                 ((memq 'draft flags)   'mu4e-draft-face)
                 ((or (memq 'unread flags) (memq 'new flags))
                  'mu4e-unread-face)
