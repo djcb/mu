61d61566abc041c744465e5f66740b49f20be6b4
Dirk-Jan C. Binnema <djcb@djcbsoftware.nl>
2021-01-23 17:43:18 +0200

    mu-msg-sexp: Improve list-post regexp
        This should catch some more addresses.

--- lib/mu-msg-sexp.c
+++ lib/mu-msg-sexp.c
@@ -177,7 +177,8 @@ maybe_append_list_post (GString *gstr, MuMsg *msg)
 	if (!list_post)
 		return;
 
-	rx = g_regex_new ("<?mailto:([a-z0-9%+@._-]+)>?", G_REGEX_CASELESS, 0, NULL);
+	rx = g_regex_new ("<?mailto:([a-z0-9!@#$%&'*+-/=?^_`{|}~]+)>?",
+                          G_REGEX_CASELESS, (GRegexMatchFlags)0, NULL);
 	g_return_if_fail(rx);
 
 	if (g_regex_match (rx, list_post, 0, &minfo)) {
