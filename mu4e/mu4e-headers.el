djcb/mu

commit 50f6f539f5e54c6bdc9abd5c6895e8f3856bf0a5

Ensure mu4e~headers-move honor line-move-ignore-invisible
This change is needed because forward-line doesn't honor this variable, more
generally visual lines.

Using `next-line` instead of `forward-line` allows this but it is more focused
on interactive use, so use `line-move` which handles visual lines without
warnings and return 0 or 1 just like `forward-line`.

mu4e/mu4e-headers.el
@@ -1843,26 +1843,32 @@ backward (if LINES is negative). If this succeeds, return the new
 docid. Otherwise, return nil."
   (unless (eq major-mode 'mu4e-headers-mode)
     (mu4e-error "Must be in mu4e-headers-mode (%S)" major-mode))
-  (let* ((_succeeded (zerop (forward-line lines)))
-         (docid (mu4e~headers-docid-at-point)))
-    ;; move point, even if this function is called when this window is not
-    ;; visible
-    (when docid
-      ;; update all windows showing the headers buffer
-      (walk-windows
-       (lambda (win)
-         (when (eq (window-buffer win) (mu4e-get-headers-buffer))
-           (set-window-point win (point))))
-       nil t)
-      (if (eq mu4e-split-view 'single-window)
-          (when (eq (window-buffer) (mu4e-get-view-buffer))
-            (mu4e-headers-view-message))
-        ;; update message view if it was already showing
-        (when (and mu4e-split-view (window-live-p mu4e~headers-view-win))
-          (mu4e-headers-view-message)))
-      ;; attempt to highlight the new line, display the message
-      (mu4e~headers-highlight docid)
-      docid)))
+  (cl-flet ((goto-next-line
+             (arg)
+             (condition-case _err
+                 (and (line-move arg) 0)
+               ((beginning-of-buffer end-of-buffer)
+                1))))
+    (let* ((_succeeded (zerop (goto-next-line lines)))
+           (docid (mu4e~headers-docid-at-point)))
+      ;; move point, even if this function is called when this window is not
+      ;; visible
+      (when docid
+        ;; update all windows showing the headers buffer
+        (walk-windows
+         (lambda (win)
+           (when (eq (window-buffer win) (mu4e-get-headers-buffer))
+             (set-window-point win (point))))
+         nil t)
+        (if (eq mu4e-split-view 'single-window)
+            (when (eq (window-buffer) (mu4e-get-view-buffer))
+              (mu4e-headers-view-message))
+          ;; update message view if it was already showing
+          (when (and mu4e-split-view (window-live-p mu4e~headers-view-win))
+            (mu4e-headers-view-message)))
+        ;; attempt to highlight the new line, display the message
+        (mu4e~headers-highlight docid)
+        docid))))
 
 (defun mu4e-headers-next (&optional n)
   "Move point to the next message header.

commit d394295fe728014b21ff06032022e2aea8ff4e49

Fix #2044: broken header movement with visual-line-mode
This fixes the issue introduced by 50f6f539 where header navigation
would break when `visual-line-mode' was enabled. Previously,
`forward-line' was used in `mu4e-view-headers-next', which disregarded
visual lines and moved by logical lines, but this was changed to
`line-move', which moves by visual lines when `line-move-visual' is
non-nil (the default when `visual-line-mode' is enabled). Thus, when
the current header line was wrapped and a message was open in the
split view, `mu4e-view-headers-next' would move to the next visual
line in the headers buffer (the same message), and then jump back to
the start of the previous line, preventing switching to the next
message.

This would also throw off navigation when `mu4e-view-headers-next' was
used with a prefix argument, since it would move by visual lines and
not headers.

`line-move-visual' is therefore set to nil before using `line-move' to
prevent these issues.

mu4e/mu4e-headers.el
@@ -1846,7 +1846,8 @@ docid. Otherwise, return nil."
   (cl-flet ((goto-next-line
              (arg)
              (condition-case _err
-                 (and (line-move arg) 0)
+                 (and (let (line-move-visual)
+                        (line-move arg)) 0)
                ((beginning-of-buffer end-of-buffer)
                 1))))
     (let* ((_succeeded (zerop (goto-next-line lines)))