78ef8ddcbb94d05ac0344755fe177fb068334bec
Dirk-Jan C. Binnema <djcb@djcbsoftware.nl>
2021-07-31 17:51:19 +0300

    mu-store: fix warning schema mismatch message
    Fixes: #2069.

--- lib/mu-store.cc
+++ lib/mu-store.cc
@@ -291,7 +291,6 @@ Store::Store (const std::string& path, bool readonly):
 {
         if (metadata().schema_version != ExpectedSchemaVersion)
                 throw Mu::Error(Error::Code::SchemaMismatch,
-                                "expected schema-version %s, but got %s",
                                 "expected schema-version %s, but got %s; "
                                 "please use 'mu init'",
                                 ExpectedSchemaVersion,
