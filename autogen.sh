975604d3ee4c9d76d72220802b6b22dc4a958662
Dirk-Jan C. Binnema <djcb@djcbsoftware.nl>
2021-08-28 11:25:05 +0300

    build: make meson the default build system
    We still support autotools for now, but it is no longer the default.

--- autogen.sh
+++ autogen.sh
@@ -1,34 +1,29 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
+echo "*** meson build setup"
+
 test -f mu/mu.cc || {
     echo "*** Run this script from the top-level mu source directory"
     exit 1
 }
 
-# opportunistically; usually not needed, but occasionally it'll
-# avoid build errors that would otherwise confuse users.
-test -f Makefile && {
-    echo "*** clear out old things"
-    make distclean 2> /dev/null
-}
+BUILDDIR=build
 
-
-command -V autoreconf > /dev/null
+command -v meson 2> /dev/null
 if [ $? != 0 ]; then
-    echo "*** No autoreconf found, please install it ***"
+    echo "*** No meson found, please install it ***"
     exit 1
 fi
 
-rm -f config.cache
-rm -rf autom4te.cache
-
-autoreconf --force --install --verbose || exit $?
-
-if test -z "$*"; then
-    echo "# Configuring without parameters"
+# we could remove build/ but let's avoid rm -rf risks...
+if test -d ${BUILDDIR}; then
+    meson --reconfigure ${BUILDDIR}
 else
-   echo "# Configure with parameters $*"
+    meson ${BUILDDIR} $@
 fi
 
-./configure --config-cache $@
+# Add a Makefile with some useful target
+cp Makefile.meson Makefile
+
+echo "*** Now run 'ninja -C ${BUILDDIR}' to build mu"
