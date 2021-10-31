5e55ef7c79d3c718b10000e0bade548fe0dd8fc9
Dirk-Jan C. Binnema <djcb@djcbsoftware.nl>
2021-10-31 13:02:11 +0200

    mu4e: finalize mu4e-query-rewrite-function migration
    There was one left.

--- mu4e/mu4e.el
+++ mu4e/mu4e.el
@@ -140,7 +140,7 @@ invoke
   (mu4e--server-ping
    (mapcar ;; send it a list of queries we'd like to see read/unread info for
     (lambda (bm)
-      (funcall (or mu4e-search-query-rewrite-function #'identity)
+      (funcall (or mu4e-query-rewrite-function #'identity)
                (plist-get bm :query)))
     ;; exclude bookmarks that are not strings, and with certain flags
     (seq-filter (lambda (bm)
