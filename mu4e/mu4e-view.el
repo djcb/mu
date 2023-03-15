djcb/mu

commit c23dad70586bbb54891c506629f2ce2ed8e463d2

mu4e-view: use with-selected-window in mv--in-headers-context
Fixed #1394

mu4e/mu4e-view.el
@@ -158,22 +158,22 @@ Then, display the results."
        (with-current-buffer buffer
          ;; make sure to select the window, or jumping won't
          ;; be reflected.
-         (select-window (get-buffer-window))
-         (if (or (mu4e~headers-goto-docid docid)
-                 ;; TODO: Is this the best way to find another
-                 ;; relevant docid for a view buffer?
-                 ;;
-                 ;; If you attach a view buffer to another headers
-                 ;; buffer that does not contain the current docid
-                 ;; then `mu4e~headers-goto-docid' returns nil and we
-                 ;; get an error. This "hack" instead gets its
-                 ;; now-changed headers buffer's current message as a
-                 ;; docid
-                 (mu4e~headers-goto-docid
-                  (with-current-buffer buffer
-                    (mu4e-message-field (mu4e-message-at-point) :docid))))
-             ,@body
-           (mu4e-error "Cannot find message in headers buffer"))))))
+         (with-selected-window (get-buffer-window)
+           (if (or (mu4e~headers-goto-docid docid)
+                   ;; TODO: Is this the best way to find another
+                   ;; relevant docid for a view buffer?
+                   ;;
+                   ;; If you attach a view buffer to another headers
+                   ;; buffer that does not contain the current docid
+                   ;; then `mu4e~headers-goto-docid' returns nil and we
+                   ;; get an error. This "hack" instead gets its
+                   ;; now-changed headers buffer's current message as a
+                   ;; docid
+                   (mu4e~headers-goto-docid
+                    (with-current-buffer buffer
+                      (mu4e-message-field (mu4e-message-at-point) :docid))))
+               ,@body
+             (mu4e-error "Cannot find message in headers buffer")))))))
 
 (defun mu4e-view-headers-next (&optional n)
   "Move point to the next message header.

commit 7f2225ccfc13d2df5b5fd8acd8eb019d9c0d3a0a

Don't die if the headers buffer isn't in a window
If the headers buffer isn't in a live window (e.g., mu4e-split-view
is 'single-window), get-buffer-window will return nil. In turn,
with-selected-window will die if fed a nil window, so default to using the
current window in this case.

This fixes a regression from c23dad70586bbb54891c506629f2ce2ed8e463d2 that
prevented using n/p from a view buffer.

mu4e/mu4e-view.el
@@ -155,10 +155,12 @@ Then, display the results."
             (docid (mu4e-message-field msg :docid)))
        (unless docid
          (mu4e-error "Message without docid: action is not possible"))
-       (with-current-buffer buffer
-         ;; make sure to select the window, or jumping won't
-         ;; be reflected.
-         (with-selected-window (get-buffer-window)
+
+       ;; make sure to select the window if possible, or jumping won't be
+       ;; reflected.
+       (with-selected-window (or (get-buffer-window buffer)
+                                 (get-buffer-window))
+         (with-current-buffer buffer
            (if (or (mu4e~headers-goto-docid docid)
                    ;; TODO: Is this the best way to find another
                    ;; relevant docid for a view buffer?