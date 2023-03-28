ba6c164ac0b95a70dad0237d1b558e10ac5491a3
Sean Farley <sean@farley.io>
2023-03-28 08:47:01 -0700

    mu4e: create random name for newly detached views
    Without creating some kind of a temporary name (which could then be
    trivially renamed by the user as the current instructions say) a new
    user trying to learn how to detach a view will run into this error when
    switching to a new message after detaching:
    
    [mu4e] Detached view buffer from *mu4e-headers*
    funcall-interactively: No buffer named *mu4e-article*<2>
    
    It seems like a good default and less friction to create a random name
    automatically when detaching.

--- mu4e/mu4e-view.el
+++ mu4e/mu4e-view.el
@@ -327,11 +327,14 @@ Add this function to `mu4e-view-mode-hook' to enable this feature."
   (unless mu4e-linked-headers-buffer
     (mu4e-error "This view buffer is already detached."))
   (mu4e-message "Detached view buffer from %s"
-                (prog1 mu4e-linked-headers-buffer
+                (progn mu4e-linked-headers-buffer
                   (with-current-buffer mu4e-linked-headers-buffer
                     (when (eq (selected-window) mu4e~headers-view-win)
                       (setq mu4e~headers-view-win nil)))
-                  (setq mu4e-linked-headers-buffer nil))))
+                  (setq mu4e-linked-headers-buffer nil)
+                  ;; automatically rename mu4e-view-article buffer when
+                  ;; detaching; will get renamed back when reattaching
+                  (rename-buffer (make-temp-name (buffer-name)) t))))
 
 (defun mu4e-view-attach (headers-buffer)
   "Attaches a view buffer to a headers buffer."
