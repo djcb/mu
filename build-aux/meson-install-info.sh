b01973205defb57b8ec58ede8ec3257cdf1a2813
Dirk-Jan C. Binnema <djcb@djcbsoftware.nl>
2023-09-03 17:33:08 +0300

    infodir: allow absolute path
    Fixes #2548

--- build-aux/meson-install-info.sh
+++ build-aux/meson-install-info.sh
@@ -4,11 +4,10 @@ infodir=$1
 infofile=$2
 
 # Meson post-install script to update info metadata
+
 # If DESTDIR is set, do _not_ install-info, since it's only a temporary
 # install
 if test -z "${DESTDIR}"; then
-    install-info --info-dir ${MESON_INSTALL_DESTDIR_PREFIX}/${infodir} \
-		 ${MESON_INSTALL_DESTDIR_PREFIX}/${infodir}/${infofile}
+    install-info --info-dir "${infodir}" "${infodir}/${infofile}"
+    gzip --force "${infodir}/${infofile}"
 fi
-
-gzip --force ${MESON_INSTALL_DESTDIR_PREFIX}/${infodir}/${infofile}
