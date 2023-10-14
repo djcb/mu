48cb83b427269418bcca8a42e45558b50a910129
Dirk-Jan C. Binnema <djcb@djcbsoftware.nl>
2023-10-14 16:46:05 +0300

    build: path improvements
    Use correct paths in date.py
    
    Fix some build warnings

--- autogen.sh
+++ autogen.sh
@@ -11,16 +11,16 @@ BUILDDIR=build
 
 command -v meson 2> /dev/null
 if [ $? != 0 ]; then
-    echo "*** No meson found, please install it ***"
+    echo "*** 'meson' not found, please install it ***"
     exit 1
 fi
 
 # we could remove build/ but let's avoid rm -rf risks...
 if test -d ${BUILDDIR}; then
-    meson --reconfigure ${BUILDDIR} $@
+    meson setup --reconfigure ${BUILDDIR} $@ || exit 1
 else
-    meson ${BUILDDIR} $@
+    meson setup ${BUILDDIR} $@ || exit 1
 fi
 
-echo "*** Now run 'ninja -C ${BUILDDIR}' to build mu"
-echo "*** Or check the Makefile for some useful targets"
+echo "*** Now run either 'ninja -C ${BUILDDIR}' or 'make' to build mu"
+echo "*** Check the Makefile for other useful targets"
--- build-aux/date.py
+++ build-aux/date.py
@@ -1,9 +1,15 @@
-#!/bin/env python
-#The MacOS 'date' is not quite up to GNU standards
+#!/usr/bin/env python3
+
+"""
+Script to get date strings, since the MacOS 'date' is not quite up to GNU
+standards
+
+E.g..
+  date.py 2023-10-14 "The year-month is %y %m"
+"""
+
 import sys
 from datetime import datetime
+
 date=datetime.strptime(sys.argv[1],'%Y-%m-%d')
 print(date.strftime(sys.argv[2]))
-#
-#  ./date.py 2023-10-14 "The year-month is %y %m"
-#
