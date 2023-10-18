451303193a63ef20d04e8a1b12fb5a8d75dbeab2
reindert <info@codesensei.nl>
2023-10-18 16:25:52 +0200

    Fix completion w other keys than quick key

--- mu4e/mu4e-helpers.el
+++ mu4e/mu4e-helpers.el
@@ -241,7 +241,7 @@ Return the matching choice value (cdr of the cell)."
              ;; Require confirmation, if the input does not match a suggestion
              nil t nil nil nil))))
     (or quick-result
-        (cdr (assoc result choices)))))
+        (cdadr (assoc result choices)))))
 
 (defun mu4e--read-choice-builtin (prompt choices)
   "Read and return one of CHOICES, prompting for PROMPT.
