daf303d444408f424248c66b01218f57a98ae22e
Dirk-Jan C. Binnema <djcb@djcbsoftware.nl>
2023-12-04 19:52:19 +0200

    mu4e-window: fix mu4e--get-current-buffer-type
    The change for emacs 30.x does not work for older emacs versions...

--- mu4e/mu4e-window.el
+++ mu4e/mu4e-window.el
@@ -152,7 +152,8 @@ tested."
 
 (defun mu4e--get-current-buffer-type ()
   "Return an internal symbol that corresponds to each mu4e major mode."
-  (cond ((derived-mode-p '(mu4e-view-mode mu4e-raw-view-mode)) 'view)
+  (cond ((or (derived-mode-p 'mu4e-view-mode)
+             (derived-mode-p 'mu4e-raw-view-mode)) 'view)
         ((derived-mode-p 'mu4e-headers-mode) 'headers)
         ((derived-mode-p 'mu4e-compose-mode) 'compose)
         ((derived-mode-p 'mu4e-main-mode) 'main)
