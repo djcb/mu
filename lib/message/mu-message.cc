df0a33b97c967df961839a6c422234cc86c9f829
Dirk-Jan C. Binnema <djcb@djcbsoftware.nl>
2024-03-01 16:31:52 +0200

    message: make sure HAVE_CLD2 is found
    This broke the language detection code

--- lib/message/mu-message.cc
+++ lib/message/mu-message.cc
@@ -1,5 +1,5 @@
 /*
-** Copyright (C) 2022-2023 Dirk-Jan C. Binnema <djcb@djcbsoftware.nl>
+** Copyright (C) 2022-2024 Dirk-Jan C. Binnema <djcb@djcbsoftware.nl>
 **
 ** This program is free software; you can redistribute it and/or modify it
 ** under the terms of the GNU General Public License as published by the
@@ -16,7 +16,7 @@
 ** Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 **
 */
-
+#include "config.h"
 
 #include "mu-message.hh"
 #include "gmime/gmime-references.h"
